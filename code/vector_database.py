e25c703fc38ebfd6c588b9daf89c98b55bbcae5f
Soumadip-Saha <soumadipsaha2000@gmail.com>
2024-02-22 19:02:10 +0530

    Add similarity_search method to VectorDatabase class. Add index mapping for new index creation.

--- code/vector_database.py
+++ code/vector_database.py
@@ -1,6 +1,9 @@
 import requests
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from typing import List
+
+DEFAULT_INDEX_MAPPING = "code\INDEX.json"  
 
 class VectorDatabase:
     def __init__(self, host, index_name, user_name, password):
@@ -8,12 +11,17 @@ class VectorDatabase:
         self.index_name = index_name
     
     def create_index(self, mapping=None):
+        if mapping is None:
+            with open(DEFAULT_INDEX_MAPPING, "r") as index_file:
+                mapping = index_file.read().strip()
+        
         if self.es.indices.exists(index=self.index_name):
             print(f"Index {self.index_name} already exists.")
             if input("Do you want to delete it? (y/n): ").lower() == "y":
                 self.es.indices.delete(index=self.index_name)
             else:
                 return False
+
         print(f"Creating index {self.index_name}")
         _ = self.es.indices.create(index=self.index_name, body=mapping)
         return True
@@ -27,3 +35,34 @@ class VectorDatabase:
             return True
         else:
             return info
+    
+    def similarity_search(self, query_vector: List[float], top_k=5):
+        script_query = {
+            "script_score": {
+                "query": {"match_all": {}},
+                "script": {
+                    "source": "cosineSimilarity(params.query_vector, 'embedding') + 1.0",
+                    "params": {"query_vector": query_vector}
+                }
+            }
+        }
+
+        response = self.es.search(
+            index=self.index_name, 
+            body={
+                "size": top_k,
+                "query": script_query,
+                "_source": ["content", "metadata.source"],
+            }
+        )
+
+        documents = [
+            {
+                "content": hit["_source"]["content"],
+                "source": hit["_source"]["metadata"]["source"],
+                "score": hit["_score"]
+            }
+            for hit in response["hits"]["hits"]
+        ]
+
+        return documents
